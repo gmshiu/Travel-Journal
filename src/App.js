df620b9561616094e6f2a9a114a889e3f22f8272
Godwin Mshiu <mshiu95@gmail.com>
2022-01-25 22:56:46 -0500

    Footer
    Added a footer into the page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Places from "./components/Places"
+import Footer from "./components/Footer"
 import data from "./data"
 import "./style.css"
 
@@ -19,6 +20,7 @@ export default function App() {
         <section className="places-list">
             {places}
         </section>
+        <Footer />
         </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,31 @@
+
 import React from "react";
+import { SocialIcon } from 'react-social-icons';
 
 export default function Footer() {
     return (
-        <footer>
-            <p>Made by Godwin Mshiu</p>
+        <footer className="footer">
+           <div className="socialMedia">
+           <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="links"
+            href="https://github.com/gmshiu"
+           > 
+           <SocialIcon url="https://github.com/gmshiu" className="github"/>
+           </a>
+           <a>
+            <SocialIcon url="https://linkedin.com/in/godwin-mshiu"/>
+           </a>
+           <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="links"
+           >
+           <SocialIcon url="https://twitter.com/G_mshiu" className="socialIcons1"   />
+            
+           </a>
+           </div>
         </footer>
-    )
+    );
 }
