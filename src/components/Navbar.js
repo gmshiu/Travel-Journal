ba984f937487f0bb5cb48488d4db4770cf92828f
Godwin Mshiu <mshiu95@gmail.com>
2022-01-28 00:28:51 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div className="navBar">
             <img src="../images/globe.png" className="nav--logo"  />
-            <h2 className="nav--text">My Travel Journal ✈️</h2>
+            <h2 className="nav--text">✈️ My Travel Journal ✈️</h2>
         </div>
     )
 }
